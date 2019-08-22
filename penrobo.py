7bcd115138a658a3482c61ee760e5d93d817030f
Takaya Kinjo <takaya.kinjo@gmail.com>
2019-08-22 23:52:30 +0900

    update

--- penrobo.py
+++ penrobo.py
@@ -1,7 +1,9 @@
+from __future__ import division
 import math
 import numpy as np
-from time import sleep
 import cv2
+import time
+import Adafruit_PCA9685
 
 pen = 0 # 0:up or 1:down
 
@@ -13,15 +15,27 @@ s1y = 400
 s2x = 125+130
 s2y = 400
 
-speed = 6
+speed = 2
 
 img2 = np.ones((450, 450, 3), np.uint8)*255
 
+# servo settings usgin PCA9685
+servo = Adafruit_PCA9685.PCA9685()
+
+servo_min = 150  # Min pulse length out of 4096
+servo_max = 600  # Max pulse length out of 4096
+servo_mid = int((servo_max+servo_min)/2)
+
+# Set frequency to 60hz, good for servos.
+servo.set_pwm_freq(60)
+
+pwm_pen_up = 200
+pwm_pen_down = 400
+
 class penRobo:
 
-    def start(x, im):
+    def start(x):
         print("Start penrobo")
-        cv2.imshow("penRobo", im)
         cv2.rectangle(img2, (20, 20), (425, 260), (0,0,0), thickness=1)
         cv2.circle(img2, (s1x, s1y), 10, (0,0,0), 2)
         cv2.circle(img2, (s2x, s2y), 10, (0,0,0), 2)
@@ -33,26 +47,21 @@ class penRobo:
         #print("Pen up")
         global pen
         pen = 0
-
+        servo.set_pwm(2, 0, pwm_pen_up)
+        
     def pen_down(x):
         global pen
         if pen == 0:
             #print("Pen down")
             pen = 1
+            servo.set_pwm(2, 0, pwm_pen_down)
 
     def pen_up_if_nessesary(x1, y1):
         global px, py
         if px != x1 or py != y1:
             penRobo.pen_up(0)
-
         
-    def move_servo(x, im, x1, y1):
-        if pen == 1:
-            cv2.circle(im, (int(x1), int(y1)), 3, (0,0,0), -1)
-        else:
-            cv2.circle(im, (int(x1), int(y1)), 3, (200,200,200), -1)
-            
-        cv2.imshow("penRobo", im)
+    def move_servo(x, x1, y1):
 
         x2 = (x1/2) + 20
         y2 = (y1/2) + 40
@@ -65,9 +74,34 @@ class penRobo:
         cv2.imshow("penRobo2", img2)
         
         cv2.waitKey(1)
-        #sleep(0.01)        
+
+        dx1 = (x2-s1x)*2
+        dy1 = (y2-s1y)*2
+        d1 = math.sqrt(dx1*dx1+dy1*dy1)
+        if dy1 != 0:
+            angle1 = -math.degrees(math.atan(dx1/dy1))
+        else:
+            angle1 = 0
+            
+        dx2 = (x2-s2x)*2
+        dy2 = (y2-s2y)*2
+        d2 = math.sqrt(dx2*dx2+dy2*dy2)
+        if dy2 != 0:
+            angle2 = -math.degrees(math.atan(dx2/dy2))
+        else:
+            angle2 = 0
+        print(angle1, angle2, d1, d2)
+
+        # TODO: calc actual angle!!!
+        pwm_s1 = int(servo_mid - angle1*9)
+        servo.set_pwm(0, 0, pwm_s1)
+        pwm_s2 = int(servo_mid - angle2*9)
+        servo.set_pwm(1, 0, pwm_s2)
+
+        #time.sleep(0.1)
+
         
-    def move_to(x, im, x1, y1):
+    def move_to(x, x1, y1):
         global px, py
         dx = x1 - px
         dy = y1 - py
@@ -80,7 +114,7 @@ class penRobo:
                     step = -speed
                 for xx in range(px, x1, step):
                     yy = py + dydx*(float(xx-px))
-                    penRobo.move_servo(0, im, xx, yy)
+                    penRobo.move_servo(0, xx, yy)
             else:
                 dxdy = float(dx)/float(dy)
                 if py < y1:
@@ -89,42 +123,42 @@ class penRobo:
                     step = -speed
                 for yy in range(py, y1, step):
                     xx = px + dxdy*(float(yy-py))
-                    penRobo.move_servo(0, im, xx, yy)
+                    penRobo.move_servo(0, xx, yy)
 
-        penRobo.move_servo(0, im, x1, y1) # make sure for the last
+        penRobo.move_servo(0, x1, y1) # make sure for the last
         
         px = x1
         py = y1
         # print("move to (",px,", ",py,")")
         
-    def point(x, im, x1, y1):
+    def point(x, x1, y1):
         penRobo.pen_up_if_nessesary(x1, y1)
-        penRobo.move_to(0, im, x1, y1)
+        penRobo.move_to(0, x1, y1)
         penRobo.pen_down(0)
-        penRobo.move_servo(0, im, x1, y1)
+        penRobo.move_servo(0, x1, y1)
 
-    def line(x, im, x1, y1, x2, y2):
+    def line(x, x1, y1, x2, y2):
         penRobo.pen_up_if_nessesary(x1, y1)
-        penRobo.move_to(0, im, x1, y1)
+        penRobo.move_to(0, x1, y1)
         penRobo.pen_down(0)
-        penRobo.move_to(0, im, x2, y2)
+        penRobo.move_to(0, x2, y2)
 
-    def rect(x, im, x1, y1, x2, y2):
+    def rect(x, x1, y1, x2, y2):
         penRobo.pen_up_if_nessesary(x1, y1)
-        penRobo.move_to(0, im, x1, y1)
+        penRobo.move_to(0, x1, y1)
         penRobo.pen_down(0)
-        penRobo.move_to(0, im, x1, y2)
-        penRobo.move_to(0, im, x2, y2)
-        penRobo.move_to(0, im, x2, y1)
-        penRobo.move_to(0, im, x1, y1)
+        penRobo.move_to(0, x1, y2)
+        penRobo.move_to(0, x2, y2)
+        penRobo.move_to(0, x2, y1)
+        penRobo.move_to(0, x1, y1)
             
-    def circle(x, im, x1, y1, r, a1, a2):
+    def circle(x, x1, y1, r, a1, a2):
 
         xx = math.sin(math.radians(a1))*r + x1
         yy = -math.cos(math.radians(a1))*r + y1
 
         penRobo.pen_up_if_nessesary(int(xx), int(yy))
-        penRobo.move_to(0, im, int(xx), int(yy))
+        penRobo.move_to(0, int(xx), int(yy))
 
         penRobo.pen_down(0)
 
@@ -138,30 +172,28 @@ class penRobo:
         while ct < cte:
             xx = math.sin(math.radians(ag))*r + x1
             yy = -math.cos(math.radians(ag))*r + y1
-            penRobo.move_to(0, im, int(xx), int(yy))
+            penRobo.move_to(0, int(xx), int(yy))
             ag += step
             ct = ct+1
 
         xx = math.sin(math.radians(a2))*r + x1
         yy = -math.cos(math.radians(a2))*r + y1
-        penRobo.move_to(0, im, int(xx), int(yy))
+        penRobo.move_to(0, int(xx), int(yy))
         
-    def erase(x, im):
+    def erase(x):
         print("erasing...")
-        cv2.rectangle(im, (0, 0), (400, 400), (255, 255, 255), thickness=-1)
         cv2.rectangle(img2, (20, 20), (425, 260), (255,255,255), thickness=-1)
         cv2.rectangle(img2, (20, 20), (425, 260), (0,0,0), thickness=1)
-        cv2.imshow("penRobo", im)
         cv2.imshow("penRobo2", img2)
         cv2.waitKey(10)
 
         
-    def number(x, im, num):
+    def number(x, num):
         if num == 0:
-            penRobo.circle(0, im, 200, 200, 150, 0, 360)
+            penRobo.circle(0, 200, 200, 150, 0, 360)
         elif num == 1:
-            penRobo.line(0, im, 200,50,200,350)
+            penRobo.line(0, 200,50,200,350)
         elif num == 2:
-            penRobo.circle(0, im, 200, 150, 100, -90, 180)
-            penRobo.line(0, im, 200, 250, 100, 350)
-            penRobo.line(0, im, 100, 350, 300, 350)
+            penRobo.circle(0, 200, 150, 100, -90, 180)
+            penRobo.line(0, 200, 250, 100, 350)
+            penRobo.line(0, 100, 350, 300, 350)
--- ryuto.py
+++ ryuto.py
@@ -5,22 +5,21 @@ import cv2
 import penrobo
 
 def hare():
-	global img
 	print("hare no ugoki")
 	# kawari ni e wo kaku
 	#im = Image.new('RGB', (400, 400), (255, 255, 255))
 	#draw = ImageDraw.Draw(im)
 
 	#draw.ellipse((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
-	pr.circle(img, 200, 200, 100, 0, 360)
+	pr.circle(200, 200, 100, 0, 360)
 	#draw.line((200, 0, 200, 60), fill=(0, 0, 0), width=1)
-	pr.line(img, 200, 60, 200, 0)	
+	pr.line(200, 60, 200, 0)	
 	#draw.line((399, 200, 340, 200), fill=(0, 0, 0), width=1)
-	pr.line(img, 399, 200, 340, 200)	
+	pr.line(399, 200, 340, 200)	
 	#draw.line((200, 399, 200, 340), fill=(0, 0, 0), width=1)
-	pr.line(img, 200, 399, 200, 340)
+	pr.line(200, 399, 200, 340)
 	#draw.line((0, 200, 60, 200), fill=(0, 0, 0), width=1)
-	pr.line(img, 0, 200, 60, 200)
+	pr.line(0, 200, 60, 200)
 	#im.save('hare.jpg', quality=95)
 
 def ame():
@@ -33,9 +32,9 @@ def ame():
 	# draw.rectangle((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
 	#draw.line((60, 200, 200, 80), fill=(0, 0, 0), width=1)
 	#draw.line((340, 200, 200, 80), fill=(0, 0, 0), width=1)
-	pr.circle(img, 200, 200, 100, -90, 90)
-	pr.line(img, 300, 200, 100, 200)
-	pr.line(img, 200, 200, 200, 320)
+	pr.circle(200, 200, 100, -90, 90)
+	pr.line(300, 200, 100, 200)
+	pr.line(200, 200, 200, 320)
 	im.save('ame.jpg', quality=95)
 
 def kumori():
@@ -44,10 +43,10 @@ def kumori():
 	# kawari ni e wo kaku
 	im = Image.new('RGB', (400, 400), (255, 255, 255))
 	draw = ImageDraw.Draw(im)
-	pr.circle(img, 200, 180, 80, -90, 90)
-	pr.circle(img, 280, 260, 80, 0, 180)
-	pr.line(img, 280, 340, 120, 340)
-	pr.circle(img, 120, 260, 80, -180, 0)
+	pr.circle(200, 180, 80, -90, 90)
+	pr.circle(280, 260, 80, 0, 180)
+	pr.line(280, 340, 120, 340)
+	pr.circle(120, 260, 80, -180, 0)
 	# draw.ellipse((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
 	# draw.rectangle((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
 	# draw.line((100, 0, 400, 0), fill=(255, 255, 255), width=1)
@@ -64,6 +63,25 @@ def yuki():
 	# draw.rectangle((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
 	draw.line((120, 190, 30, 100), fill=(0, 0, 0), width=1)
 	im.save('yuki.jpg', quality=95)
+
+def kaminari():
+	global img
+	print("kaminari no ugoki")
+	# kawari ni e wo kaku
+	im = Image.new('RGB', (400, 400), (255, 255, 255))
+	draw = ImageDraw.Draw(im)
+	# draw.ellipse((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
+	# draw.rectangle((100, 100, 300, 300), fill=(255, 255, 255), outline=(0, 0, 0))
+	#draw.line((60, 200, 200, 80), fill=(0, 0, 0), width=1)
+	#draw.line((340, 200, 200, 80), fill=(0, 0, 0), width=1)
+	# pr.circle(200, 200, 100, -90, 90)
+	pr.line(220, 60, 120, 200)
+	pr.line(120, 200, 190, 200)
+	pr.line(190, 200, 120, 320)
+	pr.line(120, 320, 260, 180)
+	pr.line(260, 180, 180, 180)
+	pr.line(180, 180, 220, 60)
+	im.save('kaminari.jpg', quality=95)
     
 url = 'http://weather.livedoor.com/forecast/webservice/json/v1'
 payload = {'city':'130010'}
@@ -78,9 +96,8 @@ print(tenki_data['forecasts'][1]['temperature']['max']['celsius'])
 tenki = tenki_data['forecasts'][0]['telop']
 
 # penrobo start
-img = np.ones((400, 400, 3), np.uint8)*255
 pr = penrobo.penRobo()
-pr.start(img)
+pr.start()
 
 
 # if '曇' in tenki:
@@ -91,11 +108,14 @@ pr.start(img)
 
 # test
 hare()
-pr.erase(img)
+pr.erase()
 ame()
-pr.erase(img)
+pr.erase()
 kumori()
-# yuki()
+pr.erase()
+yuki()
+pr.erase()
+kaminari()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
